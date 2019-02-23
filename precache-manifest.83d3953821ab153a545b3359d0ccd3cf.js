6983a980f6fcafea5fdd17eeaf67ed0e5223c7f5
diegobeltran97 <diegoelcairo@gmail.com>
2019-02-23 18:38:16 -0500

    Updates

---
+++ precache-manifest.83d3953821ab153a545b3359d0ccd3cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c39b81db3ea42e9379b8",
+    "url": "/course-react/static/js/runtime~main.c39b81db.js"
+  },
+  {
+    "revision": "001c33fd3c796339a517",
+    "url": "/course-react/static/js/main.001c33fd.chunk.js"
+  },
+  {
+    "revision": "702c479e90a92263fb9f",
+    "url": "/course-react/static/js/1.702c479e.chunk.js"
+  },
+  {
+    "revision": "001c33fd3c796339a517",
+    "url": "/course-react/static/css/main.dd62f639.chunk.css"
+  },
+  {
+    "revision": "702c479e90a92263fb9f",
+    "url": "/course-react/static/css/1.596d2c9a.chunk.css"
+  },
+  {
+    "revision": "4123c7bd9d73ccb1ff9d9d2f687c3d6f",
+    "url": "/course-react/index.html"
+  }
+];
